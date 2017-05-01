953b6711c3bcf85259e11b55432b5182391ebb7a
mho8 <mho8@pitt.edu>
2017-04-30 22:32:57 -0400

    Add files via upload

--- JS/js_proj1.js
+++ JS/js_proj1.js
@@ -13,6 +13,84 @@ document.getElementById("cartButton").onclick = function ()
 	
 document.getElementById("addButton1").onclick = function ()
 {
-  document.getElementById("added1").innerHTML = 'Item has been added to your cart.';
-  document.getElementById("addButton1").innerHTML = 'REMOVE';
+  if (document.getElementById("addButton1").innerHTML == 'ADD')
+  {
+    document.getElementById("added1").innerHTML = 'Item has been added to your cart.';
+    document.getElementById("addButton1").innerHTML = 'REMOVE';
+  }
+  else if (document.getElementById("addButton1").innerHTML == 'REMOVE')
+  {
+    document.getElementById("added1").innerHTML = 'Item has been removed from your cart.';
+    document.getElementById("addButton1").innerHTML = 'ADD';
+  }
+};
+
+document.getElementById("addButton2").onclick = function ()
+{
+  if (document.getElementById("addButton2").innerHTML == 'ADD')
+  {
+    document.getElementById("added2").innerHTML = 'Item has been added to your cart.';
+    document.getElementById("addButton2").innerHTML = 'REMOVE';
+  }
+  else if (document.getElementById("addButton2").innerHTML == 'REMOVE')
+  {
+    document.getElementById("added2").innerHTML = 'Item has been removed from your cart.';
+    document.getElementById("addButton2").innerHTML = 'ADD';
+  }
+};
+
+document.getElementById("addButton3").onclick = function ()
+{
+  if (document.getElementById("addButton3").innerHTML == 'ADD')
+  {
+    document.getElementById("added3").innerHTML = 'Item has been added to your cart.';
+    document.getElementById("addButton3").innerHTML = 'REMOVE';
+  }
+  else if (document.getElementById("addButton3").innerHTML == 'REMOVE')
+  {
+    document.getElementById("added3").innerHTML = 'Item has been removed from your cart.';
+    document.getElementById("addButton3").innerHTML = 'ADD';
+  }
+};
+
+document.getElementById("addButton4").onclick = function ()
+{
+  if (document.getElementById("addButton4").innerHTML == 'ADD')
+  {
+    document.getElementById("added4").innerHTML = 'Item has been added to your cart.';
+    document.getElementById("addButton4").innerHTML = 'REMOVE';
+  }
+  else if (document.getElementById("addButton4").innerHTML == 'REMOVE')
+  {
+    document.getElementById("added4").innerHTML = 'Item has been removed from your cart.';
+    document.getElementById("addButton4").innerHTML = 'ADD';
+  }
+};
+
+document.getElementById("addButton5").onclick = function ()
+{
+  if (document.getElementById("addButton5").innerHTML == 'ADD')
+  {
+    document.getElementById("added5").innerHTML = 'Item has been added to your cart.';
+    document.getElementById("addButton5").innerHTML = 'REMOVE';
+  }
+  else if (document.getElementById("addButton5").innerHTML == 'REMOVE')
+  {
+    document.getElementById("added5").innerHTML = 'Item has been removed from your cart.';
+    document.getElementById("addButton5").innerHTML = 'ADD';
+  }
+};
+
+document.getElementById("addButton6").onclick = function ()
+{
+  if (document.getElementById("addButton6").innerHTML == 'ADD')
+  {
+    document.getElementById("added6").innerHTML = 'Item has been added to your cart.';
+    document.getElementById("addButton6").innerHTML = 'REMOVE';
+  }
+  else if (document.getElementById("addButton6").innerHTML == 'REMOVE')
+  {
+    document.getElementById("added6").innerHTML = 'Item has been removed from your cart.';
+    document.getElementById("addButton6").innerHTML = 'ADD';
+  }
 };
